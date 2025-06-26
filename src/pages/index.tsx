4dc0a74696829bd8861ff913136e542a2eee3260
rajuvemula <rj.vemula81@gmail.com>
2025-06-26 22:59:47 +0530

    fix: sanity fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -145,7 +145,7 @@ const Home:NextPage = () => {
                         <div className="bg-primary-400 px-4 py-2 flex items-center justify-between rounded-t-2xl mb-4">
                             <div className="flex items-center gap-2">
                                 <div className="w-2 h-2 rounded-full bg-white animate-pulse" />
-                                <span className="text-sm font-semibold">Position: {position}</span>
+                                <span className="text-sm font-semibold">Transferring Amount: {amount || "0"}</span>
                             </div>
                             <span className="text-sm font-semibold"></span>
                         </div>
